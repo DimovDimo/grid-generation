5e9927d94462920df85c3e90b1bbe9f14299da3b
DimovDimo <d.dimov1996@gmail.com>
2020-01-18 16:03:38 +0200

    added function gridLines

--- app/resources/js/grid-generation.js
+++ app/resources/js/grid-generation.js
@@ -1,12 +1,27 @@
-(function gridButton(){
+(function gridButton() {
     let button = document.getElementById("grid-button");
     button.addEventListener("click", gridButtonClick);
 }());
 
-function gridButtonClick(){
-    let grid = document.getElementById("grid");
+function gridButtonClick() {
+    let grid = getGridElement();
+    gridBackgroundColor(grid);
+    gridLines(grid);
+}
+
+gridButtonClick();
+
+function getGridElement() {
+    return document.getElementById("grid");
+}
+
+function gridBackgroundColor(grid) {
     let backgroundColor = document.getElementById("background-color-input-id");
     grid.style.setProperty("background-color", backgroundColor.value);
 }
 
-gridButtonClick();
+function gridLines(grid) {
+    let verticalLinesColor = document.getElementById("vertical-lines-color-input-id");
+    let linearGradients = `linear-gradient(${verticalLinesColor.value} 100px, transparent 100px)`;
+    grid.style.setProperty("background-image", linearGradients);
+}
