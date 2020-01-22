738d13c93be96648161546dc5a1f0f50bca84ae8
DimovDimo <d.dimov1996@gmail.com>
2020-01-22 10:37:19 +0200

    added small vertical & horizontal lines

--- app/resources/js/grid-generation.js
+++ app/resources/js/grid-generation.js
@@ -7,6 +7,8 @@ function gridChange() {
     let grid = getGridElement();
     gridBackgroundColor(grid);
     gridLines(grid);
+    gridBackgroundSize(grid);
+    gridBackgroundPosition(grid);
 }
 
 gridChange();
@@ -22,11 +24,15 @@ function gridBackgroundColor(grid) {
 
 function gridLines(grid) {
     let linearGradients = 
-        `${LinearGradient("vertical-lines-color-input-id", "vertical-lines-width-input-id", "vertical-lines-transparent-input-id", false)},
-         ${LinearGradient("horizontal-lines-color-input-id", "horizontal-lines-width-input-id", "horizontal-lines-transparent-input-id", true)}`;
+        `${linearGradient("vertical-lines-color-input-id", "vertical-lines-width-input-id", "vertical-lines-transparent-input-id", false)},
+         ${linearGradient("horizontal-lines-color-input-id", "horizontal-lines-width-input-id", "horizontal-lines-transparent-input-id", true)},
+         ${linearGradient("small-vertical-lines-color-input-id", "small-vertical-lines-width-input-id", "small-vertical-lines-transparent-input-id", false)},
+         ${linearGradient("small-horizontal-lines-color-input-id", "small-horizontal-lines-width-input-id", "small-horizontal-lines-transparent-input-id", true)}`;
+    
+    grid.style.setProperty("background-image", linearGradients);
 }
 
-function LinearGradient(ColorID, WidthPixelsID, TransparentPixelsID, isHorizontal){
+function linearGradient(ColorID, WidthPixelsID, TransparentPixelsID, isHorizontal){
     let Color = document.getElementById(ColorID);
     let WidthPixels = document.getElementById(WidthPixelsID);
     let TransparentPixels = document.getElementById(TransparentPixelsID);
@@ -44,4 +50,12 @@ function verticalLinesLinearGradient(Color, WidthPixels, TransparentPixels){
 
 function horizontalLinesLinearGradient(Color, WidthPixels, TransparentPixels){
     return `linear-gradient(90deg, ${Color.value} ${WidthPixels.value}px, transparent ${TransparentPixels.value}px)`;
+}
+
+function gridBackgroundSize(grid){
+
+}
+
+function gridBackgroundPosition(grid){
+    
 }
